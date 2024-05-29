7cf30c64b21224065d032377eea5e69ba3b22307
Ronin Shawanda <roninsha@student.21-school.ru>
2024-05-29 14:09:06 +0500

    Add config to work with DB

--- roll-app/main.py
+++ roll-app/main.py
@@ -1,11 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(api_router)
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+  # startup
+  yield
+  # shutdow
+  await db_helper.dispose()
+
+app = FastAPI(
+  lifespan=lifespan,
+)
+app.include_router(
+  api_router,
+  prefix=settings.api.prefix
+)
 
 if __name__ == "__main__":
   uvicorn.run(
